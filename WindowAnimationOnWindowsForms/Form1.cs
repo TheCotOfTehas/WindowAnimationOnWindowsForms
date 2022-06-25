cb0b4eb74389f5eeb8bab6991b87b1f3de7b366e
THECOTOFTEHAS\tsebr <tsebroevgeny@yandex.ru>
2022-06-25 17:46:15 +0300

    Добавил вариант анимации с амплетудой

--- WindowAnimationOnWindowsForms/Form1.cs
+++ WindowAnimationOnWindowsForms/Form1.cs
@@ -2,32 +2,48 @@ namespace WindowAnimationOnWindowsForms
 {
     public partial class Form1 : Form
     {
+        bool workingForm;
+        bool oldAnimation;
         public Form1()
         {
             InitializeComponent();
 
             panel2.MouseEnter += async (x, y) =>
             {
-                while (panel2.Location.X > panel1.Location.X + 15)
+                while (!workingForm && panel2.Location.X > panel1.Location.X + 15)
                 {
+                    workingForm = true;
+
                     await Task.Delay(1);
-                    panel2.Location = new Point(panel2.Location.X - 1, panel2.Location.Y);
+                    if (!oldAnimation)
+                        panel2.Location = new Point(panel2.Location.X - 1, panel2.Location.Y);
+                    else
+                        panel2.Location = new Point(panel2.Location.X - panel2.Location.X / 8, panel2.Location.Y);
+
+                    workingForm = false;
                 }
             };
 
             panel1.MouseEnter += async (x, y) =>
             {
-                while (panel2.Location.X != panel2.Width + 15)
+                while (!workingForm && panel2.Location.X <= panel2.Width)
                 {
+                    workingForm = true;
+
                     await Task.Delay(1);
-                    panel2.Location = new Point(panel2.Location.X + 1, panel2.Location.Y);
+                    if (!oldAnimation)
+                        panel2.Location = new Point(panel2.Location.X + 1, panel2.Location.Y);
+                    else
+                        panel2.Location = new Point(panel2.Location.X + (panel2.Width - panel2.Location.X) / 8, panel2.Location.Y);
+
+                    workingForm = false;
                 }
             };
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            oldAnimation = !oldAnimation;
         }
     }
 }
